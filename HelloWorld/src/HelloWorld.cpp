f7395bb07a902af04048125f4847f886f2b0937d
sachin <sachin@idea-PC>
2018-02-11 15:07:03 -0800

    helloworld you are awesome!

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : nm
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!hello iamAashi mathur!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
