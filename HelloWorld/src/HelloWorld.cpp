c1e88206c30b395f8114d3ff59d5f751ea7447ec
sachin <sachin@idea-PC>
2018-02-11 22:08:34 -0800

    updated display message

--- HelloWorld/src/HelloWorld.cpp
+++ HelloWorld/src/HelloWorld.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!hello world first application!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!hello world first application *******!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
